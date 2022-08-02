e3ab13f0c2b8a5e6bcf54122717b283c7cec2c45
apanay20 <antreas20197@gmail.com>
2022-08-03 02:23:43 +0300

    Add NavMeshAgentComponents + Basic Wander Behavior

---
+++ Assets/BasicInteraction/My Assets/Scripts/AI/WanderBehavior.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Cyens.ReInherit
+{
+    public class WanderBehavior : MonoBehaviour
+    {
+        private Vector3 m_TargetPosition;
+        private NavMeshAgent m_NavMeshAgent;
+
+        private void OnEnable()
+        {
+            m_NavMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+            GetNewDestination();
+        }
+
+        private void GetNewDestination()
+        {
+            m_TargetPosition = new Vector3(UnityEngine.Random.Range(0f, 24f), 0f, UnityEngine.Random.Range(0f, 24f));
+            while (!m_NavMeshAgent.SetDestination(m_TargetPosition)) {
+                m_TargetPosition = new Vector3(UnityEngine.Random.Range(0f, 24f), 0f, UnityEngine.Random.Range(0f, 24f));
+            }
+        }
+
+        private bool CheckIfArrived()
+        {
+            if (!m_NavMeshAgent.pathPending) {
+                if (m_NavMeshAgent.remainingDistance <= m_NavMeshAgent.stoppingDistance) {
+                    if (!m_NavMeshAgent.hasPath || m_NavMeshAgent.velocity.sqrMagnitude == 0f) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
