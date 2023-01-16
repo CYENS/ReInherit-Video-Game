fb8193e753a5dba750e7fb6eb3b622e0fb528c19
cothon01 <cothon01@cs.ucy.ac.cy>
2023-01-16 10:42:35 +0200

    Applied IK rig to Keeper prefab

---
+++ Assets/Source/Gameplay/Artifact/ArtifactProxy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cyens.ReInherit
+{
+    /// <summary>
+    /// Attached to an object that represents the artifact.
+    /// Useful in cases where a collision event hits a collider that doesn't have the artifact attached to it
+    /// </summary>
+    public class ArtifactProxy : MonoBehaviour
+    {
+
+        private Artifact m_artifact;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            m_artifact = GetComponentInParent<Artifact>();
+        }
+    }
+}
