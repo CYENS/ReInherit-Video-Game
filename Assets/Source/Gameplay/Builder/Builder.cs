25b7502da89c0ec97fca0fa20f4bc66a20847206
Andreas Panayiotou <antreas20197@gmail.com>
2023-01-24 17:52:59 +0200

    Implemented Builders Logic

--- Assets/Source/Gameplay/Builder/Builder.cs
+++ Assets/Source/Gameplay/Builder/Builder.cs
@@ -4,26 +4,60 @@ using System.Collections.Generic;
 using Cyens.ReInherit.Gameplay.Management;
 using UnityEngine;
 using Pathfinding;
+using Pathfinding.RVO;
+using Unity.VisualScripting;
 
 namespace Cyens.ReInherit
 {
     public class Builder : MonoBehaviour
     {
-        public enum State {Idle, Ready, MovingToRoom, MovingToRoomPoint, Building, Returning }
+        public enum State {Idle, Ready, MovingToRoom, MovingToRoomPoint, Building, FormingQueue, Returning }
         private enum BuilderType {Carpenter, Concreter, Plumber, Electrician, Welder}
 
         private BuilderManager m_builderManager;
         private List<Renderer> m_renderers;
         private AIPath m_aiPath;
+        [SerializeField] private int m_builderIndex;
+        [SerializeField] private Task m_currentTask;
+        [SerializeField] private bool m_isMainBuilder;
+        [SerializeField] private Builder m_MainBuilder;
+        [SerializeField] private Builder m_forntBuilder;
         [SerializeField] private State m_state = State.Ready;
         [SerializeField] private BuilderType m_builderType;
         [SerializeField] private float m_buildingTimer = 0f;
         [SerializeField] private float m_idleTimer = 0;
-        private Vector3 m_roomEndPoint;
+        private RVOController m_rvo;
 
+        public enum Goal { BuildNewRoom = 0  }
+
+        public Vector3 GetRoomPoint(int builderIndex)
+        {
+            return m_currentTask.roomPoints[builderIndex];
+        }
+
+        public State GetState()
+        {
+            return m_state;
+        }
+        public int GetBuilderIndex()
+        {
+            return m_builderIndex;
+        }
+        
+        [System.Serializable]
+        public class Task
+        {
+            public Goal goal;
+            public Vector3 room;
+            public List<Vector3> roomPoints;
+        }
+        
         // Start is called before the first frame update
         void Start()
         {
+            m_rvo = GetComponent<RVOController>();
+            if (m_isMainBuilder)
+                m_rvo.enabled = true;
             m_builderManager = BuilderManager.Instance; 
             m_aiPath = GetComponent<AIPath>();
             FindRenderers();
@@ -58,20 +92,39 @@ namespace Cyens.ReInherit
         }
 
         /// <summary>
+        /// Helper function to make usage more convenient.
+        /// </summary>
+        private void EnableRenderers() => EnableDisableRenderers(true);
+
+        /// <summary>
+        /// Helper function to make usage more convenient.
+        /// </summary>
+        private void DisableRenderers() => EnableDisableRenderers(false);
+        
+        /// <summary>
         /// Execute builder logic
         /// </summary>
-        private void ExecuteBuilderLogic()
+        private void ExecuteMainBuilderLogic()
         {
             switch (m_state) {
-                //Builder is ready to be assign to a room; check for new task
+                //Builder is ready to be assigned to a room; check for new task
                 case State.Ready: {
+                    if (m_builderManager.IsNewTaskAvailable() == false) return;
+                    if (m_builderManager.CheckAllBuildersSaneState(State.Ready) == false) return;
+                    
+                    m_currentTask = m_builderManager.GetNextTask();
+                    SetMovePosition(m_currentTask.room);
+                    m_rvo.enabled = true;
+                    m_state = State.MovingToRoom;
+                    EnableRenderers();
+
                     break;
                 }
                 //Builder moving towards room; check if arrived at place
                 case State.MovingToRoom: {
-                    EnableDisableRenderers(true);
-                    if (m_aiPath.remainingDistance < 4f && m_aiPath.pathPending == false) {
-                        SetMovePosition(m_roomEndPoint);
+                    if (m_aiPath.remainingDistance < 2f && m_aiPath.pathPending == false) {
+                        m_builderManager.SetLastQueue();
+                        SetMovePosition(GetRoomPoint(m_builderIndex));
                         m_state = State.MovingToRoomPoint;
                     }
                     break;
@@ -88,22 +141,33 @@ namespace Cyens.ReInherit
                 case State.Building: {
                     if (m_buildingTimer >= m_builderManager.GetBuildingDelay()) {
                         Vector3 basePoint = m_builderManager.GetBasePosition();
-                        SetMovePosition(basePoint);
-                        m_state = State.Returning;
+                        SetMovePosition(m_builderManager.GetLastQueuePos(m_builderIndex));
+                        m_state = State.FormingQueue;
+                    }
+                    break;
+                }
+                //Builder taking its place in queue to return
+                case State.FormingQueue: {
+                    if (m_aiPath.remainingDistance < 0.1f && m_aiPath.pathPending == false) {
+                        if(m_builderManager.CheckAllBuildersRemainingDistance(0.2f)){
+                            SetMovePosition(m_builderManager.GetBasePosition());
+                            m_state = State.Returning;
+                        }
                     }
                     break;
                 }
                 //Builder is returning to base; check if arrived
                 case State.Returning: {
-                    if (m_aiPath.remainingDistance < 0.5f  && m_aiPath.pathPending == false) {
+                    if (m_aiPath.remainingDistance < 0.2f && m_aiPath.pathPending == false) {
                         m_state = State.Idle;
                         m_idleTimer = 0f;
-                        EnableDisableRenderers(false);
                     }
                     break;
                 }
                 //Builder is idling; check if timer passed for becoming ready
                 case State.Idle: {
+                    DisableRenderers();
+                    m_rvo.enabled = false;
                     if (m_idleTimer >= m_builderManager.GetIdleDelay()) {
                         m_state = State.Ready;
                     }
@@ -112,53 +176,84 @@ namespace Cyens.ReInherit
             }
         }
 
+        private void ExecuteSimpleBuilderLogic()
+        {
+            if(m_MainBuilder.GetState() != State.Idle && m_MainBuilder.GetState() != State.Ready)
+                m_state = m_MainBuilder.GetState();
+            switch (m_state) {
+                case State.Ready: {
+                    DisableRenderers();
+                    break;
+                }
+                //Builder moving towards room; check if arrived at place
+                case State.MovingToRoom: {
+                    float disToFront = Vector3.Distance(transform.position, m_forntBuilder.transform.position);
+                    if (disToFront > 1f) {
+                        EnableRenderers();
+                        m_rvo.enabled = true;
+                        SetMovePosition(m_forntBuilder.transform.position);
+                    }
+
+                    break;
+                }
+                //Builder moving specific point in room; check if arrived at place
+                case State.MovingToRoomPoint: {
+                    SetMovePosition(m_MainBuilder.GetRoomPoint(m_builderIndex));
+                    break;
+                }
+                //Builder is currently building; check if done
+                case State.Building: {
+                    break;
+                }
+                //Builder taking its place in queue to return
+                case State.FormingQueue: {
+                    SetMovePosition(m_builderManager.GetLastQueuePos(m_builderIndex));
+                    break;
+                }
+                //Builder is returning to base; check if arrived
+                case State.Returning: {
+                    if (Vector3.Distance(transform.position, m_builderManager.GetBasePosition()) < 2.75f) {
+                        m_state = State.Ready;
+                        m_rvo.enabled = false;
+                        return;
+                    }
+                    SetMovePosition(m_forntBuilder.transform.position);
+                    break;
+                }
+                case State.Idle: {
+                    break;
+                }
+            }
+        }
+
         // Update is called once per frame
         void Update()
         {
-            ExecuteBuilderLogic();
+            if (m_isMainBuilder)
+                ExecuteMainBuilderLogic();
+            else
+                ExecuteSimpleBuilderLogic();
         }
 
         private void FixedUpdate()
         {
-            //Timers
-            if (m_state == State.Building)
-                m_buildingTimer += Time.fixedDeltaTime;
-            else
-                m_buildingTimer = 0f;
-            
-            if (m_state == State.Idle)
-                m_idleTimer += Time.fixedDeltaTime;
-            else
-                m_idleTimer = 0f;
-        }
+            if (m_isMainBuilder) {
+                if (m_state == State.Building)
+                    m_buildingTimer += Time.fixedDeltaTime;
+                else
+                    m_buildingTimer = 0f;
 
-        public void SetState(State state)
-        {
-            m_state = state;
-        }
-
-        public State GetState()
-        {
-            return m_state;
-        }
-
-        public void SetRoomEndPoint(Vector3 point)
-        {
-            m_roomEndPoint = point;
+                if (m_state == State.Idle)
+                    m_idleTimer += Time.fixedDeltaTime;
+                else
+                    m_idleTimer = 0f;
+            }
         }
 
         private void SetMovePosition(Vector3 position)
         {
             m_aiPath.destination = position;
             m_aiPath.SearchPath();
-        }
-        
-        public IEnumerator SetMovePosition(Vector3 position, float seconds, State state)
-        {
-            yield return new WaitForSeconds(seconds);
-            m_aiPath.destination = position;
-            m_aiPath.SearchPath();
-            m_state = state;
         }
     }
 }
--- Assets/Source/Gameplay/Management/BuilderManager.cs
+++ Assets/Source/Gameplay/Management/BuilderManager.cs
@@ -1,26 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Cyens.ReInherit.Patterns;
+using UnityEditor;
 using UnityEngine;
+using Pathfinding;
 
 namespace Cyens.ReInherit.Gameplay.Management
 {
     public class BuilderManager : Singleton<BuilderManager>
     {
-        public Queue<List<Vector3>> m_roomPoints = new Queue<List<Vector3>>();
+        public Queue<Builder.Task> m_builderTasks = new Queue<Builder.Task>();
         [SerializeField] private Transform m_buildersParent;
         private List<Builder> m_builders;
-        [SerializeField] [Tooltip("Number of rooms left to build.")]
+        private List<Vector3> m_lastQueue;
+        [SerializeField] [Tooltip("Number of task left for the builders.")]
         private int m_tasksLeft = 0;
         [SerializeField] private Transform m_basePosition;
         [SerializeField] private Transform m_RoomTestPoints;
-        [SerializeField] [Tooltip("Seconds to wait for building the room.")]
+        [SerializeField] [Tooltip("Seconds to wait for building")]
         private float m_buildingDelay;
         [SerializeField] [Tooltip("Seconds to wait/idling until next task.")]
         private float m_idleDelay;
-        [SerializeField] [Tooltip("Seconds to wait between each builder to start moving")]
-        private float m_spawnDelay;
 
         public float GetBuildingDelay()
         {
@@ -31,13 +33,67 @@ namespace Cyens.ReInherit.Gameplay.Management
         {
             return m_idleDelay;
         }
-        
-        public void AddNewTask(List<Vector3> points)
+
+        public bool CheckAllBuildersSaneState(Builder.State state)
         {
-            m_roomPoints.Enqueue(points);
+            foreach (var builder in m_builders) {
+                if (builder.GetState() != state)
+                    return false;
+            }
+            return true;
+        }
+
+        public bool CheckAllBuildersRemainingDistance(float threshold)
+        {
+            foreach (var builder in m_builders) {
+                if (builder.GetComponent<AIPath>().remainingDistance > threshold)
+                    return false;
+            }
+            return true;
+        }
+
+        public void SetLastQueue()
+        {
+            foreach (var builder in m_builders) {
+                m_lastQueue[builder.GetBuilderIndex()] = builder.transform.position;
+            }
+            m_lastQueue.Reverse();
+        }
+
+        public Vector3 GetLastQueuePos(int index)
+        {
+            return m_lastQueue[index];
+        }
+
+        private void AddNewTask(Vector3 roomCenter, List<Vector3> roomPoints, Builder.Goal goal)
+        {
+            var task = new Builder.Task();
+            task.room = roomCenter;
+            task.roomPoints = roomPoints;
+            task.goal = goal;
+
+            m_builderTasks.Enqueue(task);
             m_tasksLeft += 1;
         }
+
+        public void AddBuildTask(Vector3 target, List<Vector3> points) => AddNewTask(target, points, Builder.Goal.BuildNewRoom);
+
+
+        public bool IsNewTaskAvailable()
+        {
+            if (m_builderTasks.Count > 0)
+                return true;
+            return false;
+        }
         
+        public Builder.Task GetNextTask()
+        {
+            m_tasksLeft -= 1;
+            if(m_builderTasks.Count > 0)
+                return m_builderTasks.Dequeue();
+            return null;
+        }
+
         public Vector3 GetBasePosition()
         {
             return m_basePosition.position;
@@ -49,71 +105,28 @@ namespace Cyens.ReInherit.Gameplay.Management
         }
         
         private void Start()
-        {
+        { 
+            m_lastQueue = new List<Vector3>(m_buildersParent.childCount);
+            for (int i = 0; i < m_buildersParent.childCount; i++) {
+                m_lastQueue.Add(Vector3.zero);
+            }
             m_builders = new List<Builder>(m_buildersParent.childCount);
-            foreach (Transform child in m_buildersParent) {
-                m_builders.Add(child.GetComponent<Builder>());
-            }
-        }
-
-        /// <summary>
-        /// Check if all builders are ready to start a new task
-        /// </summary>
-        private bool AllBuildersReady()
-        {
-            foreach (Builder b in m_builders) {
-                if (b.GetState() != Builder.State.Ready)
-                    return false;
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Run Builder logic. First set the destination of all builders to the
-        /// first point in list(Room center). Then each builder will go to a specific point in the room.
-        /// This is to ensure that all builder will follow same path to arrive at the room.
-        /// ALso add a small spawn delay so they move in a queue.
-        /// </summary>
-        private void RunBuilderManagerLogic()
-        {
-            if (m_tasksLeft <= 0) 
-                return; 
-            if (AllBuildersReady() == false)
-                return;
-            
-            m_tasksLeft -= 1;
-            List<Vector3> points = m_roomPoints.Dequeue();
-            for (int i = 0; i < m_builders.Count; i++) {
-                Builder b = m_builders[i];
-                b.SetRoomEndPoint(points[i + 1]);
-                StartCoroutine(b.SetMovePosition(points[0], m_spawnDelay * i, Builder.State.MovingToRoom));
+            foreach (Transform builder in m_buildersParent) {
+                m_builders.Add(builder.GetComponent<Builder>());
             }
         }
 
         private void Update()
         {
-            RunBuilderManagerLogic();
-            
             //--TESTING, add active points from hierarchy to the task queue
-            foreach (Transform child in m_RoomTestPoints) {
-                if (child.gameObject.activeInHierarchy) {
-                    List<Vector3> points = new List<Vector3>(child.childCount);
-                    points.Add(child.position);
-                    foreach (Transform point in child) {
+            foreach (Transform room in m_RoomTestPoints) {
+                if (room.gameObject.activeInHierarchy) {
+                    List<Vector3> points = new List<Vector3>(5);
+                    foreach (Transform point in room)
                         points.Add(point.position);
-                    }
-                    AddNewTask(points);
-                    Destroy(child.gameObject);
+                    AddBuildTask(room.position, points);
+                    Destroy(room.gameObject);
                 }
-            }
-        }
-
-        void OnDrawGizmos()
-        {
-            foreach (Transform points in m_RoomTestPoints) {
-                Gizmos.color = new Color(0, 1, 0 , 1f);
-                foreach (Transform point in points)
-                    Gizmos.DrawCube(point.position, new Vector3(0.5f, 0.2f, 0.5f));
             }
         }
     }
